3ff7f6dde6e5e3c8ce5cca54f5f2e81d134390d1
Rodrigo Xavier <rodrfx@gmail.com>
2022-09-08 12:42:41 -0300

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from './styles/global';
-import { defaultTheme } from './styles/themes/default';
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export const App = () => {
-	return (
-		<ThemeProvider theme={defaultTheme}>
-			<p>app</p>
-			<GlobalStyle />
-		</ThemeProvider>
-	);
-};
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <p>app</p>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
