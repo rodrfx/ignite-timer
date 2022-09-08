5da4466abbd0ba23953fffeddbc1b15b510c2d12
Rodrigo Xavier <rodrfx@gmail.com>
2022-09-08 14:53:38 -0300

    application routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <p>app</p>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
-    </ThemeProvider>
+    </ThemeProvider >
   )
 }
---
+++ src/components/pages/History.tsx
@@ -0,0 +1,7 @@
+export const History = () => {
+    return (
+        <>
+            <p>History</p>
+        </>
+    )
+}
